30799883bfdae586b27f76b1132ce422ab2b20c0
James Gong <zifei@MacBook-Pro.local>
2016-08-04 01:31:53 +1000

    landing page

---
+++ app/scripts/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+	$('#fullpage').fullpage({
+		anchors: ['1', '2', '3', '4'],
+		sectionsColor: ['#81d0e1', '#f0f7f9', '#ffbe6a', '#f5f5f0'],
+		navigation: true,
+		navigationPosition: 'right',
+		navigationTooltips: ['', '', '', '']
+	});
+});
