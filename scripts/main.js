508900a2456c8520d46a24a38dc7dfdddd6202cf
iamzifei <iamzifei@gmail.com>
2016-09-30 01:34:05 +0000

    Update site

---
+++ scripts/main.js
@@ -0,0 +1,1 @@
+"use strict";$(document).ready(function(){$("#fullpage").fullpage({anchors:["1","2","3","4"],sectionsColor:["#FED340","#f0f7f9","#8867AB","#f5f5f0"],navigation:!0,navigationPosition:"right",navigationTooltips:["","","",""],afterRender:function(){$("video").get(0).play()}})});
